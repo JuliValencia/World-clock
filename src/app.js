625a4acf0ad173cefe728c6ae9d22328a9c44dd3
JuliValencia <juliana.valencia1@gmail.com>
2023-04-13 20:42:52 +0200

    Updated UpdateCity function

--- src/app.js
+++ src/app.js
@@ -31,29 +31,29 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
-
-  function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current") {
-      cityTimeZone = moment.tz.guess();
-    }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-      "A"
-    )}</small></div>
+    "A"
+  )}</small></div>
   </div>
   <a href = "/">🔙</a>
   `;
-  }
 }
+
 updateTime();
 setInterval(updateTime, 1000);
 
